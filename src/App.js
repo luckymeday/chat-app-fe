f0afd41ed45884f6b1048e21564403409338c2b4
Khoa <damanhkhoa@gmail.com>
2020-09-07 15:12:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import socket from "./socket";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [messages, setMessages] = React.useState([]);
+  const [user, setUser] = React.useState(null)
+  const messagesRef = React.useRef([])
+
+  React.useEffect(() => {
+    socket.on("connection")
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  React.useEffect(() => {
+    const name = prompt("What is your name");
+    setUser(name)
+  }, [])
+
+  React.useEffect(() => {
+    socket.on("message", function (msg) {
+      const k = uuidv4()
+      messagesRef.current = [...messagesRef.current, {
+        user: msg.user,
+        message: msg.message,
+        id: k
+      }]
+      setMessages(messagesRef.current)
+    })
+  }, []);
+
+
+  const handleChatSubmit = (e) => {
+    e.preventDefault();
+    const message = e.target.chat.value
+    socket.emit("sendMessage", {
+      user,
+      message
+    })
+    const form = document.getElementById("chatform");
+    form.reset();
+  }
+
+  const renderMessages = (messages) => {
+    return messages.map(e => <Message key={e.id} uuid={e.id} obj={e} user={user} />)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleChatSubmit} id="chatform">
+        <input name="chat" placeholder="chat with me" />
+        <input type="submit" value="send message" />
+      </form>
+      {renderMessages(messages)}
+
     </div>
   );
+}
+
+
+const Message = (props) => {
+  return <p>
+    <span
+      className={props.obj.user === props.user ? 'red' : 'black'}
+      style={{ fontWeight: 'bold' }}>
+      {props.obj.user}
+    </span>
+      : {props.uuid}</p>
 }
 
 export default App;
